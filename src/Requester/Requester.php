1e01d83c64f8a4c64dfc1a53b051e8b5d04a353d
milirezai <miladrezaix@gmail.com>
2026-08-30 14:24:48 +0330

    fix bux in driver requester

--- src/Requester/Requester.php
+++ src/Requester/Requester.php
@@ -4,23 +4,28 @@ namespace Mili\Milipay\Requester;
 
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Client\RequestException;
+use Mili\Milipay\Exceptions\MilipayException;
 
 class Requester
 {
     public function post(int $timeout, int $retry, string $api, mixed $data): array
     {
-        $start = microtime(true);
+        try {
+            $start = microtime(true);
+            $response = Http::timeout($timeout)
+                ->retry($retry,150)
+                ->withHeaders([
+                    'Content-Type' => 'application/json',
+                    'Accept' => 'application/json'
+                ])->asJson()->post($api,$data);
 
-        $response = Http::timeout($timeout)
-            ->retry($retry,150)
-            ->withHeaders([
-                'Content-Type' => 'application/json',
-                'Accept' => 'application/json'
-            ])->asJson()->post($api,$data);
+            $end = microtime(true);
 
-        $end = microtime(true);
-
-        return $this->resolveResponse(start_time: $start, end_time: $end, response: $response);
+            return $this->resolveResponse(start_time: $start, end_time: $end, response: $response);
+        }catch (RequestException $e){
+            throw new MilipayException($e->getMessage(),$e->getCode());
+        }
     }
     private function resolveResponse(int $start_time, int $end_time, mixed $response): array
     {
